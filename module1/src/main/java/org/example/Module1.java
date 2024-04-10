626acb13167d441e2a881f36c344153ebd05ffd5
Alban Auzeill <alban.auzeill@sonarsource.com>
2024-04-11 00:53:55 +0200

    n/a

--- module1/src/main/java/org/example/Module1.java
+++ module1/src/main/java/org/example/Module1.java
@@ -1,8 +1,28 @@
 package org.example;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import javax.annotation.Nullable;
 
 public class Module1 {
+
+  public static void main2(String[] args) {
+    List<String> list = Stream.of("b", "c", "a")
+      .collect(Collectors.toList());
+    list.sort(Comparator.comparing(Function.identity()));
+    System.out.println(list);
+  }
+
+  public static void main(String[] args) {
+    List<String> list = Stream.of("b", "c", "a")
+      .toList();
+    list.sort(Comparator.comparing(Function.identity()));
+    System.out.println(list);
+  }
+
   @Nullable
   @Deprecated(since = "1.0")
   public static String get() {
